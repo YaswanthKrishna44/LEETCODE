57a114d24df092e8e45a55708410e7c8dec2df88
YaswanthKrishna44 <yaswanthkrishnakonala@gmail.com>
2026-08-08 01:08:19 +0530

    Time: 19 ms (99.79%), Space: 21.9 MB (56.69%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -7,5 +7,9 @@ class Solution:
             curr_sum+=num
             if (curr_sum-k) in prefix_map:
                 count+=prefix_map[curr_sum-k]
-            prefix_map[curr_sum]=prefix_map.get(curr_sum,0)+1
+            #prefix_map[curr_sum]=prefix_map.get(curr_sum,0)+1
+            if curr_sum in prefix_map:
+                prefix_map[curr_sum] += 1
+            else:
+                prefix_map[curr_sum] = 1
         return count        
