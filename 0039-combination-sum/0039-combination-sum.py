2832c4304e3a77aacfebe850c110182b7915c496
YaswanthKrishna44 <yaswanthkrishnakonala@gmail.com>
2026-08-24 22:25:16 +0530

    Time: 4 ms (84.67%), Space: 17.8 MB (100%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dice(p, nums, target, start):
+            if target == 0:
+                return [p]
+            res = []
+            for i in range(start, len(nums)):
+                num = nums[i]
+                if num <= target:
+                    res += dice(p + [num], nums, target - num, i)  # i, not 0, to avoid reordering
+            return res
+        return dice([], candidates, target, 0)
