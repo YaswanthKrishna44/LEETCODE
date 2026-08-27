4d4df5332f3e46be301a6059f93287b40b39a08e
YaswanthKrishna44 <yaswanthkrishnakonala@gmail.com>
2026-08-28 01:31:52 +0530

    Time: 4 ms (10.86%), Space: 18.1 MB (100%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[[]]
+        for num in nums:
+            new_subsets=[]
+            for subset in result:
+                new_set=subset+[num]
+                if new_set not in result:
+                    new_subsets.append(new_set)
+            result.extend(new_subsets)
+        return result
+
+
+       
